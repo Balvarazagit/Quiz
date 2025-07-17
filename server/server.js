af33b4d5e79a1ac0092b0589c319d4ef0e0164e4
Balva Raza <balvaraza2@gmail.com>
2025-07-17 03:34:01 -0700

    comment

--- server/server.js
+++ server/server.js
@@ -27,7 +27,10 @@ app.use("/api/results", require("./routes/results"));
 app.use("/api/admin", require("./routes/admin"));
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { 
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true 
+  })
   .then(() => {
     console.log("✅ MongoDB Connected");
     server.listen(process.env.PORT || 5000, () =>
